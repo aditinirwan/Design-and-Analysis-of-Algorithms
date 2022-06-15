5a5356ff17343b028fc2bd897043c4c79f5af0d8
aditinirwan <72340988+aditinirwan@users.noreply.github.com>
2022-06-15 23:07:30 +0530

    Add files via upload

---
+++ binary search - recusive.cpp
@@ -0,0 +1,31 @@
+/* Compare x with the middle element.
+If x matches with the middle element, we return the mid index.
+Else If x is greater than the mid element, then x can only lie in the right half subarray after the mid element. So we recur for the right half.
+Else (x is smaller) recur for the left half. */
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int binarySearch(int arr[], int l, int r, int x)
+{
+    if (r >= l) {
+        int mid = l + (r - l) / 2;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] > x)
+            return binarySearch(arr, l, mid - 1, x);
+        return binarySearch(arr, mid + 1, r, x);
+    }
+    return -1;
+}
+ 
+int main(void)
+{
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int x = 10;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = binarySearch(arr, 0, n - 1, x);
+    (result == -1)
+        ? cout << "Element is not present in array"
+        : cout << "Element is present at index " << result;
+    return 0;
+}
